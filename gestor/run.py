cec667a24b7db75e2bf13096a40dce099f1458ce
tereesaalvarez <alvdeporteresa@gmail.com>
2022-10-06 23:57:39 +0200

    commit

--- gestor/run.py
+++ gestor/run.py
@@ -1,3 +1,9 @@
+import sys
 import menu
+import ui
 if __name__ == "__main__":
-    menu.iniciar()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
