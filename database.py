fa531ed7a8f26dd6954ed7a2ff32eadfc9711ba2
tereesaalvarez <alvdeporteresa@gmail.com>
2022-10-06 20:09:49 +0200

    commit

---
+++ database.py
@@ -0,0 +1,30 @@
+class Cliente:
+    def __init__(self, dni, nombre, apellido):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+    def __str__(self):
+        return f"({self.dni}) {self.nombre} {self.apellido}"
+
+class Clientes:
+    # Lista de clientes
+    lista = []
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
+    def crear(dni, nombre, apellido):
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        return cliente
+    def modificar(dni, nombre, apellido):
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
+    def borrar(dni):
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(i)
+                return cliente
